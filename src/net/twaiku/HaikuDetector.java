07f409ac5270bdfe8fcd0bd57245076f0bb71cfb
Qwuke <tristan.e.mortimer@gmail.com>
2017-12-06 12:40:19 -0500

    It bothers me that/for each change to the pom file/I write a commit

--- src/net/twaiku/HaikuDetector.java
+++ src/net/twaiku/HaikuDetector.java
@@ -1,7 +1,11 @@
 package net.twaiku;
 
 import opennlp.tools.*;
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 
 public class HaikuDetector {
+	
 
 }
