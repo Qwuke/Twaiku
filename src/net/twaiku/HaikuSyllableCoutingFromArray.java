8b2a30ffd088d1d908115fffe58a539d1455cb6b
Qwuke <tristan.e.mortimer@gmail.com>
2017-12-08 13:47:41 -0500

    Added formatting/checker for sanitized tweets/megadog was here

--- src/net/twaiku/HaikuSyllableCoutingFromArray.java
+++ src/net/twaiku/HaikuSyllableCoutingFromArray.java
@@ -1,55 +1,78 @@
 package net.twaiku;
 
+import java.io.IOException;
+
+import net.twaiku.cmu.CmuDictionary;
+import net.twaiku.rhymer.Rhymer;
+
 public class HaikuSyllableCoutingFromArray {
 
-	public static void main(String[] args) {
+	public static boolean count(String[] words) throws IOException {
 
-		int[] arr = { 1, 1, 3, 3, 2, 1, 1, 1, 1, 3 };
+		//int[] arr = { 1, 1, 3, 3, 2, 1, 1, 1, 1, 3 };
 		int i = 0;
-
-		int sc = 0;
-		int line1 = 5;
-		int line2 = 12;
-		int line3 = 17;
+		Rhymer rhymer = CmuDictionary.loadRhymer();
+		int syllableCount = 0;
+		final int line1 = 5;
+		final int line2 = 12;
+		final int line3 = 17;
+		int firstSyllableNum=0;
+		int secondSyllableNum=0;
+		int thirdSyllableNum=0;
 
 		// Starting point
-		for (i = 0; sc <= 17; i++) {
-			if (sc == line1) {
+		for (i = 0; syllableCount <= 17; i++) {
+			if (words.length>i) {
+			//Use placeholder vars such that we only 
+				firstSyllableNum = rhymer.getSyllables(words[i]);
+			}
+			if (syllableCount == line1) {
 
-				System.out.println("i'm here");
-				for (int j = i; sc <= 17; j++) {
-					if (sc == line2) {
+				System.out.println("first 5 check");
+				for (int j = i; syllableCount <= 17; j++) {
+					if (words.length>j) {
+						secondSyllableNum = rhymer.getSyllables(words[j]);
+					}
+					if (syllableCount == line2) {
 
-						System.out.println("Now I'm Here");
-						for (int r = j; sc <= 17; r++) {
-							if (sc == line3) {
+						System.out.println("second 5 check");
+						for (int r = j; syllableCount <= 17; r++) {
+							//fuck
+							if (words.length>r) {
+								thirdSyllableNum = rhymer.getSyllables(words[r]);
+							}
+							if (syllableCount == line3) {
 								System.out.println("We FUCKING DID IT");
-								//sc = 18 is a break statement to step all the way out
-								sc = 18;
-							} else if ((sc + arr[r]) > line3) {
-								System.out.println("failed line 3 test");
+								return true;
+							} else if ((syllableCount + thirdSyllableNum) > line3) {
+								System.out.println("failed second 5 check");
+								return false;
 							} else {
-								sc += arr[r];
-								System.out.println(sc);
+								syllableCount += thirdSyllableNum;
+								System.out.println(syllableCount);
 							}
 						}
 
-					} else if ((sc + arr[j]) > line2) {
-						System.out.println("failed line2 test");
+					} else if ((syllableCount + secondSyllableNum) > line2) {
+						System.out.println("failed 7 check");
+						return false;
 					} else {
-						sc += arr[j];
-						System.out.println(sc);
+						syllableCount += secondSyllableNum;
+						System.out.println(syllableCount);
 					}
 
 				}
 
-			} else if ((sc + arr[i]) > line1) {
+			} else if ((syllableCount + firstSyllableNum) > line1) {
 				System.out.println("failed line1 test");
+				return false;
 			} else {
-				sc += arr[i];
-				System.out.println(sc);
+				syllableCount += firstSyllableNum;
+				System.out.println(syllableCount);
 
 			}
 		}
+		return false;
+
 	}
 }
